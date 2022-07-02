78a6a7c69722fad87d98a9d02b235ed75b86b5a5
Віктор Липкань <lipkan02@gmail.com>
2022-07-02 23:08:57 +0300

    Добавьте файлы проекта.

---
+++ PerimeterCalculator/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("===Hello, welcome to the polygon perimeter calculator===");
+
+while (true)
+{
+    Console.WriteLine("Enter count of sides");
+
+    int count = Convert.ToInt32(Console.ReadLine());
+    int sum = 0;
+
+    for (int i = 1; i <= count; i++)
+    {
+        Console.WriteLine($"Enter the number {i}");
+        int number = Convert.ToInt32(Console.ReadLine());
+        sum += number;
+    }
+
+    Console.WriteLine(sum);
+
+    Console.ReadKey();
+}
